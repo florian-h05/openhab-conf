a719f55206d48921096d1368594b4270d88dfa74
Florian Hotze <fhgames18@icloud.com>
2021-06-13 22:25:09 +0200

    Add openHAB ui-based/scripts for smarthome-log (#3)
    * Create openhab_calls-and-feature.js
    
    Signed-off-by: Florian Hotze <fhgames18@icloud.com>
    
    * Create log-alarms.js
    
    Signed-off-by: Florian Hotze <fhgames18@icloud.com>
    
    * Create log-shading.js
    
    Signed-off-by: Florian Hotze <fhgames18@icloud.com>
    
    * Update log-shading.js
    
    Signed-off-by: Florian Hotze <fhgames18@icloud.com>
    
    * Update log-shading.js
    
    Signed-off-by: Florian Hotze <fhgames18@icloud.com>

---
+++ ui-based/scripts/smarthome-log/log-shading.js
@@ -0,0 +1,36 @@
+/*
+This script logs knx shading to the InfluxDB smarthome log.
+Configure it in line 33.
+*/
+
+var deviceName, itemName
+
+var logger = Java.type('org.slf4j.LoggerFactory').getLogger('org.openhab.rule.' + ctx.ruleUID);
+var PersistenceExtension = Java.type("org.openhab.core.persistence.extensions.PersistenceExtensions");
+var ZonedDateTime = Java.type("java.time.ZonedDateTime");
+var now = ZonedDateTime.now();
+var Exec = Java.type("org.openhab.core.model.script.actions.Exec");
+var Duration = Java.type("java.time.Duration");
+
+function logShading(deviceName) {
+  itemName = deviceName;
+  itemName += '_BeschStat';
+  var previousState = PersistenceExtension.previousState(ir.getItem(itemName), false).state;
+  var actualState = itemRegistry.getItem(itemName).getState();
+  //logger.info(itemName + ' previous state is: ' + previousState);
+  //logger.info(itemName + ' actual state is: ' + actualState);
+  if ((previousState == OFF) && (actualState == ON)) {
+    Exec.executeCommandLine(Duration.ofSeconds(30), "/usr/bin/python3","/etc/openhab/scripts/openhab-log-influxdb.py","-t knx","-d " + deviceName,"-l Aut. Verschattung aktiviert.");
+    //logger.info('logged ' + deviceName + ' shading on to the smarthome-log');
+  }
+  if ((previousState == ON) && (actualState == OFF)) {
+    Exec.executeCommandLine(Duration.ofSeconds(30), "/usr/bin/python3","/etc/openhab/scripts/openhab-log-influxdb.py","-t knx","-d " + deviceName,"-l Aut. Verschattung deaktiviert.");
+    //logger.info('logged ' + deviceName + ' shading off to the smarthome-log');
+  }
+}
+
+// insert your shading item's names into the list
+var shading_list = ['shading1', 'shading2', 'shading3'];
+for (var index in shading_list) {
+  logShading(shading_list[index]);
+}
