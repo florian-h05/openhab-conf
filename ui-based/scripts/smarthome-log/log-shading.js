cc8dbab0d365d7edea94745e595d23c8714ac24b
Florian Hotze <fhgames18@icloud.com>
2021-06-14 17:55:17 +0200

    Update log-shading.js
    Rewrote the script: removed the check for changed item via persistence service, instead of this now call the script from the rule with the triggering item.
    
    Signed-off-by: Florian Hotze <fhgames18@icloud.com>

--- ui-based/scripts/smarthome-log/log-shading.js
+++ ui-based/scripts/smarthome-log/log-shading.js
@@ -1,36 +1,27 @@
 /*
 This script logs knx shading to the InfluxDB smarthome log.
-Configure it in line 33.
+No configuration needed. This script gets "this.triggerinItem" fom the rule that calls the script.
+The "Unique ID" of this script should be: "log-shading-script".
 */
 
-var deviceName, itemName
+var itemName;
 
 var logger = Java.type('org.slf4j.LoggerFactory').getLogger('org.openhab.rule.' + ctx.ruleUID);
-var PersistenceExtension = Java.type("org.openhab.core.persistence.extensions.PersistenceExtensions");
-var ZonedDateTime = Java.type("java.time.ZonedDateTime");
-var now = ZonedDateTime.now();
 var Exec = Java.type("org.openhab.core.model.script.actions.Exec");
 var Duration = Java.type("java.time.Duration");
 
-function logShading(deviceName) {
-  itemName = deviceName;
-  itemName += '_BeschStat';
-  var previousState = PersistenceExtension.previousState(ir.getItem(itemName), false, "rrd4j").state;
+function logShading(itemName) {
   var actualState = itemRegistry.getItem(itemName).getState();
-  //logger.info(itemName + ' previous state is: ' + previousState);
   //logger.info(itemName + ' actual state is: ' + actualState);
-  if ((previousState == OFF) && (actualState == ON)) {
+  var deviceName = itemName.replace('_BeschStat', '');
+  if (actualState == ON) {
     Exec.executeCommandLine(Duration.ofSeconds(30), "/usr/bin/python3","/etc/openhab/scripts/openhab-log-influxdb.py","-t knx","-d " + deviceName,"-l Aut. Verschattung aktiviert.");
     //logger.info('logged ' + deviceName + ' shading on to the smarthome-log');
   }
-  if ((previousState == ON) && (actualState == OFF)) {
+  if (actualState == OFF) {
     Exec.executeCommandLine(Duration.ofSeconds(30), "/usr/bin/python3","/etc/openhab/scripts/openhab-log-influxdb.py","-t knx","-d " + deviceName,"-l Aut. Verschattung deaktiviert.");
     //logger.info('logged ' + deviceName + ' shading off to the smarthome-log');
   }
 }
 
-// insert your shading item's names into the list
-var shading_list = ['shading1', 'shading2', 'shading3'];
-for (var index in shading_list) {
-  logShading(shading_list[index]);
-}
+logShading(this.triggeringItem);
