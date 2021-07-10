52923285dd764b88a4127bfbe8e0dc93cb6a8eda
Florian Hotze <fhgames18@icloud.com>
2021-07-10 12:51:23 +0200

    Update heatalarm.js
    Fixes #12 with:
    * Check if timer already exists, when it exists then do not touch it. If it does not exist, then create it. 
    * Added log message for creating and skipping a timer
    * Only try to cancel timer if the timer for the closed contact exists. 
    * Added log message for cancelling timer.
    
    Improvements:
    * If the heatLevel is only on warning, add a given amount of minutes to the timer time.
    
    Signed-off-by: Florian Hotze <fhgames18@icloud.com>

--- ui-based/alarms/heatalarm.js
+++ ui-based/alarms/heatalarm.js
@@ -1,24 +1,22 @@
 /*
 This script contains the logic for the rainalarms and sends the notifications.
 Configuration:
- - Names of items in lines 16-19 and treshold in line 15.
- - Names of roofwindow items in lines 106-117. String in roofwindow itemnames in line 135.
+ - Names of items in lines 17-19 and treshold in line 15.
+ - Names of roofwindow items in lines 106-123. String in roofwindow itemnames in line 145.
 The "Unique ID" of this script should be: "heatalarm-script".
 */
 
 // configuration of heatalarm
-var klLueftungTime = '2m'
-var grLueftungTime = '20m'
-var openTime = '15m'
+var klLueftungTime = 25
+var grLueftungTime = 20
+var openTime = 15
+var warningTime = 10 // time to add when its only a warning
 // Temperature treshold, positive values mean inside temp to outside. Example: 2 means at least 2 degress higher temp on the outside.
 var tempTreshold = 2
 // configuration of the itemnames
 var tempOut = itemRegistry.getItem('Aussentemperatur').getState()
 var heatLevelItem = 'Hitze_Stufe'
 var contactGroup = 'KontakteAufZu'
-
-// used global by several functions
-var groupMembers
 
 // load timer_mgr library
 this.OPENHAB_CONF = (this.OPENHAB_CONF === undefined) ? java.lang.System.getenv("OPENHAB_CONF") : this.OPENHAB_CONF
@@ -65,19 +63,19 @@ function TemperatureDifference (contactItem) {
 }
 
 // check the status and start the timer for the notification
-function StartWarning (contactItem, timerTime) {
+function StartWarning (contactItem, timerDuration) {
   var heatLevel = itemRegistry.getItem(heatLevelItem).getState().toString()
   var itemLabel = itemRegistry.getItem(contactItem).getLabel()
   var diff = TemperatureDifference(contactItem)
   if ((heatLevel >= 1) && (diff === true)) {
     // Function generator for the timerOver actions.
-    function timerOver (contactItem, itemLabel) {
+    function timerOver (contactItem, itemLabel, heatLevelItem) {
+      logger.info('Creating timer "' + contactItem + '" time: ' + timerTime)
       return function () {
-        logger.info('The timer is over. Contact item is: '+ contactItem)
         var contactState = itemRegistry.getItem(contactItem).getState().toString()
         var heatLevel = itemRegistry.getItem(heatLevelItem).getState().toString()
         var diff = TemperatureDifference(contactItem)
-        // logger.info(contactItem + ' temp treshold: ' + diff)
+        logger.info('The timer is over. Contact item is: '+ contactItem + ' temp treshold reached: ' + diff)
         if ((contactState === 'CLOSED') && (diff === true)) {
           if (heatLevel == 4) {
             NotificationAlarm(itemLabel)
@@ -87,13 +85,18 @@ function StartWarning (contactItem, timerTime) {
       }
       }
     }
+    // Generate the timer time, if heatLevel is not 4 (alarm) then add time
+    if (heatLevel == 4) {
+      var timerTime = timerDuration + 'm'
+    } else {
+      var timerTime = timerDuration + warningTime + 'm'
+    }
     // Create the Timer
-    // logger.info('Creating timer "' + contactItem + '" time: ' + timerTime)
-    this.tm.check(contactItem,
-                 timerTime,
-                 timerOver(contactItem, itemLabel),
-                 false,
-                 function () { logger.info('Timer for "' + contactItem + '" already exists, skipping!') })
+    if (this.tm.hasTimer(contactItem)) {
+      logger.info('Timer for "' + contactItem + '" already exists, skipping!')
+    } else {
+      this.tm.check(contactItem, timerTime, timerOver(contactItem, itemLabel, heatLevelItem))
+    } 
   }
 }
 
@@ -102,19 +105,23 @@ function RoofwindowAlarm (item) {
   // remove the suffix
   item = item.replace('_zu', '')
   item = item.replace('_klLueftung', '')
+  item = item.replace('_grLueftung', '')
   // retrieve the contact states from openHAB
   var StateZu = itemRegistry.getItem(item + '_zu').getState().toString()
   var StateKlLueftung = itemRegistry.getItem(item + '_klLueftung').getState().toString()
   var StateGrLueftung = itemRegistry.getItem(item + '_grLueftung').getState().toString()
   // checks for the different states.
-  if (StateZu === 'CLOSED' && StateKlLueftung === 'OPEN' && StateGrLueftung === 'OPEN') { // kleine Lüftung
-    StartWarning(item + '_zu', klLueftungTime)
+  if (StateZu === 'CLOSED' && StateKlLueftung === 'CLOSED' && StateGrLueftung === 'CLOSED') { // ganz geöffnet
+    StartWarning(item + '_zu', openTime)
   } else if (StateZu === 'CLOSED' && StateKlLueftung === 'CLOSED' && StateGrLueftung === 'OPEN') { // große Lüftung
     StartWarning(item + '_zu', grLueftungTime)
-  } else if (StateZu === 'CLOSED' && StateKlLueftung === 'CLOSED' && StateGrLueftung === 'CLOSED') { // ganz geöffnet
-    StartWarning(item + '_zu', openTime)
+  } else if (StateZu === 'CLOSED' && StateKlLueftung === 'OPEN' && StateGrLueftung === 'OPEN') { // kleine Lüftung
+    StartWarning(item + '_zu', klLueftungTime)
   } else if (StateZu === 'OPEN') { // ganz geschlossen
-    tm.cancel(item + '_zu')
+    if ( this.tm.hasTimer(item + '_zu') ) {
+      logger.info('Cancelling timer for "' + item + '_zu", contact closed.')
+      this.tm.cancel(item + '_zu')
+    }
   }
 }
 
@@ -125,11 +132,14 @@ function SingleContact (contactItem) {
   if (StateSingle === 'CLOSED') {
     StartWarning(contactItem, openTime)
   } else if (StateSingle === 'OPEN') {
-    tm.cancel(contactItem)
+    if ( this.tm.hasTimer(contactItem) ) {
+      logger.info('Cancelling timer for "' + contactItem + '", contact closed.')
+      this.tm.cancel(contactItem)
+    }
   }
 }
 
-var groupMembers= GroupUtils.getMembers(contactGroup)
+var groupMembers = GroupUtils.getMembers(contactGroup)
 for (var index in groupMembers) {
   // check whether itemname contains 'Dachfenster'
   var b = groupMembers[index].search('Dachfenster') 
