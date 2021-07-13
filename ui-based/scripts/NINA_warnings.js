c1fe2b0b729d700c2d5c336c54fd065db17c5ce1
Florian Hotze <fhgames18@icloud.com>
2021-07-13 16:38:16 +0200

    Update NINA_warnings.js
    Fixed:
    * not using all 4 items in openHAB
    * Problem with the item state when there was no warning: now only updating item from script if there is a warning.
    
    Signed-off-by: Florian Hotze <fhgames18@icloud.com>

--- ui-based/scripts/NINA_warnings.js
+++ ui-based/scripts/NINA_warnings.js
@@ -12,18 +12,22 @@ var Exec = Java.type('org.openhab.core.model.script.actions.Exec')
 var Duration = Java.type('java.time.Duration')
 
 var weather = Exec.executeCommandLine(Duration.ofSeconds(10),'bash','/etc/openhab/scripts/NINA_Warn.bash','-w')
+logger.info('Response from NINA_Warn.bash: ' + weather)
 weather = weather.split("\\r?\\n")
 
 // reset all items to None. (for the actualWeather widget)
-for (var i = 0; i < 3; i++) {
+for (var i = 0; i < 4; i++) {
   var n = i+1
   events.postUpdate('NINA_WetterWarn' + n, 'None.')
 }
 
 // post warnings to the items
-for(var i = 0; (i < weather.length) && (i < 3); i++) {
+for(var i = 0; (i < weather.length) && (i < 4); i++) {
   var text = weather[i]
   var n = i+1
-  text = text.replace('Amtliche UNWETTERWARNUNG vor ', '')
-  events.postUpdate('NINA_WetterWarn' + n, text)
+  var b = text.search('WARNUNG')
+  if ( b != -1 ) {
+    text = text.replace('Amtliche UNWETTERWARNUNG vor ', '')
+    events.postUpdate('NINA_WetterWarn' + n, text)
+  }
 }
