9c92fc6a67d72d42d6fe382e57480ecaa41940bb
Florian Hotze <fhgames18@icloud.com>
2021-08-22 19:23:10 +0200

    Update groupUtils.js
    Huge improvements and additions:
    * replaced my old, weird way of getting the items' names with cleaner
    code
    * added ability to get not only the members' names, but also labels
    and states
    * perform all arithmetic operations on Number items as natively
    supported by group items
    * count the states matching a given comparison expression

--- automation/lib/javascript/community/groupUtils.js
+++ automation/lib/javascript/community/groupUtils.js
@@ -1,59 +1,138 @@
-/*
-Get the members of a group as an array of their names.
-Tested to work with openHAB 3.0.2
-Copyright (c) 2021 Florian Hotze under MIT License
-*/
+/**
+ * Utilities for working with openHAB groups in JavaScript.
+ * This code is not compatible with the GraalVM JavaScript add-on.
+ *
+ * Tested to work with openHAB 3.1.0 stable.
+ * Copyright (c) 2021 Florian Hotze under MIT License
+ */
 
-/*
-Constructor, initializes the logger.
-*/
+/**
+ * Constructor, initializes the logger.
+ */
 var GroupUtils = function () {
-  this.log = Java.type("org.slf4j.LoggerFactory").getLogger("org.openhab.model.script.Rules.GroupUtils")
+  this.log = Java.type('org.slf4j.LoggerFactory').getLogger('org.openhab.model.script.Rules.GroupUtils')
+  this.log.debug('Building groupUtils instance.')
 }
 
-/*
-Get only the direct members of a group as an array.
-*/
-GroupUtils.prototype.getMembers = function (groupName) {
-  this.log.debug('Getting direct members of group ' + groupName)
-  var membersString = new String(ir.getItem(groupName).members)
-  var membersSplit = membersString.split(' (Type')
-  var firstMember = membersSplit[0].split('[')
-  var groupMembers = [firstMember[1]]
-  this.log.debug('Group member: ' + firstMember[1])
-  // remove the first element
-  membersSplit.splice(0, 1)
-  // remove the last element
-  membersSplit.splice(-1, 1)
-  // iterate over the rest of membersSplit and add to groupMembers
-  for (var index in membersSplit) {
-    var nMember = membersSplit[index].split(']), ')
-    groupMembers.push(nMember[1])
-    this.log.debug('Group member: ' + nMember[1])
-  }
+/**
+ * Get the direct members of a group.
+ *
+ * @param {*} group Name of the openHAB group
+ * @param {*} characteristic Which characteristic of the members you get, valid: name (default), label, state
+ */
+GroupUtils.prototype.getMembers = function (group, characteristic) {
+  this.log.debug('Getting direct members of group ' + group)
+  var groupMembers = Array()
+  ir.getItem(group).getMembers().stream().forEach(function (item) {
+    if (characteristic == 'label') {
+      groupMembers.push(item.getLabel())
+    } else if (characteristic == 'state') {
+      groupMembers.push(item.getState())
+    } else {
+      groupMembers.push(item.getName())
+    }
+  })
   // return the array
   return groupMembers
 }
 
-/*
-Get all (also childs) members of a group as an array.
-*/
-GroupUtils.prototype.getAllMembers = function (groupName) {
-  this.log.debug('Getting all members of group ' + groupName)
-  var membersString = new String(ir.getItem(groupName).allMembers)
-  var membersSplit = membersString.split(' (Type')
-  var firstMember = membersSplit[0].split('[')
-  var groupMembers = [firstMember[1]]
-  // remove the first element
-  membersSplit.splice(0, 1)  
-  // remove the last element
-  membersSplit.splice(-1, 1)
-  // iterate over the rest of membersSplit and add to groupMembers
-  for (var index in membersSplit) {
-    var nMember = membersSplit[index].split(']), ')
-    groupMembers.push(nMember[1])
-    this.log.debug('Group member: ' + nMember[1])
-  }
+/**
+ * Get all (also childs) members of a group, the group items are excluded from this list.
+ *
+ * @param {*} group Name of the openHAB group
+ * @param {*} characteristic Which characteristic of the members you get, valid: name (default), label, state
+ */
+GroupUtils.prototype.getAllMembers = function (group, characteristic) {
+  this.log.debug('Getting all members of group ' + group)
+  var groupAllMembers = Array()
+  ir.getItem(group).getAllMembers().stream().forEach(function (item) {
+    if (characteristic == 'label') {
+      groupAllMembers.push(item.getLabel())
+    } else if (characteristic == 'state') {
+      groupAllMembers.push(item.getState())
+    } else {
+      groupAllMembers.push(item.getName())
+    }
+  })
   // return the array
-  return groupMembers
+  return groupAllMembers
+}
+
+/**
+ * Perform arithmetic operations on multiple states.
+ *
+ * @param {*} items Array of states
+ * @param {*} func Arithmetic function to perform, valid: SUM, AVG, MIN, MAX
+ */
+GroupUtils.prototype.arithmetic = function (items, func) {
+  if (func == 'SUM') {
+    var sum = items[0]
+    for (var i = 1; i < items.length; i++) {
+      sum += items[i]
+    }
+    return sum
+  } else if (func == 'AVG') {
+    var sum = items[0]
+    for (var i = 1; i < items.length; i++) {
+      sum += items[i]
+    }
+    var avg = sum / items.length
+    return avg
+  } else if (func == 'MIN') {
+    var min = items[0];
+    for (var i = 1; i < items.length; ++i) {
+      if (items[i] < min) {
+        min = items[i];
+      }
+    }
+    return min
+    
+  } else if (func == 'MAX') {
+    var max = items[0];
+    for (var i = 1; i < items.length; ++i) {
+      if (items[i] > max) {
+        max = items[i];
+      }
+    }
+    return max
+  }
+}
+
+/**
+ * Returns the number of states matching the given comparison expression.
+ *
+ * @param {*} items Array of states
+ * @param {*} op Comparison operator, valid: equal, notEqual, larger, largerEqual, smaller, smallerEqual
+ * @param {*} comp State to compare with
+ */
+GroupUtils.prototype.count = function (items, op, comp) {
+  var counter = 0
+  for (var i = 0; i < items.length; ++i) {
+    if (op == 'equal') {
+      if (items[i] == comp) {
+        ++counter
+      }
+    } else if (op == 'notEqual') {
+      if (items[i] != comp) {
+        ++counter
+      }
+    } else if (op == 'larger') {
+      if (items[i] > comp) {
+        ++counter
+      }
+    } else if (op == 'largerEqual') {
+      if (items[i] >= comp) {
+        ++counter
+      }
+    } else if (op == 'smaller') {
+      if (items[i] < comp) {
+        ++counter
+      }
+    } else if (op == 'smallerEqual') {
+      if (items[i] <= comp) {
+        ++counter
+      }
+    }
+  }
+  return counter
 }
