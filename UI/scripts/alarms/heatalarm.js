8541eb88071d5ead71eef20e023c38df56bb02ae
Florian Hotze <florianh_dev@icloud.com>
2021-11-04 14:08:19 +0100

    heatalarm.js: Improvements in docs & semistandard
    * Improved documentation.
    * Added license.
    * Updated style with Java-Script Semi-Standard Style.

--- UI/scripts/alarms/heatalarm.js
+++ UI/scripts/alarms/heatalarm.js
@@ -1,153 +1,194 @@
 /*
 This script contains the logic for the rainalarms and sends the notifications.
-Configuration:
- - Names of items in lines 18-20 and treshold in line 16.
- - Names of roofwindow items in lines 107-124. String in roofwindow itemnames in line 146.
+Configuration on top of the file.
+Dependencies:
+ -  groupUtils & timerMgr from https://github.com/rkoshak/openhab-rules-tools.
 The "Unique ID" of this script should be: "heatalarm-script".
+
 Copyright (c) 2021 Florian Hotze under MIT License
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
-// configuration of heatalarm
-var klLueftungTime = 30
-var grLueftungTime = 25
-var openTime = 15
-var warningTime = 20 // time to add when its only a warning
+/*
+Configuration
+*/
+// Time until an alarm is sent.
+var klLueftungTime = 30;
+var grLueftungTime = 25;
+var openTime = 15;
+var warningTime = 20; // Time to add when its only a warning.
 // Temperature treshold, positive values mean inside temp to outside. Example: 2 means at least 2 degress higher temp on the outside.
-var tempTreshold = 2
-// configuration of the itemnames
-var tempOut = itemRegistry.getItem('Aussentemperatur').getState()
-var heatLevelItem = 'Hitze_Stufe'
-var contactGroup = 'KontakteAufZu'
+var tempTreshold = 2;
+// Configuration of the itemnames.
+var groupname = 'KontakteAufZu';
+var roofwindowString = 'Dachfenster';
+var tempOut = itemRegistry.getItem('Aussentemperatur').getState();
+var heatLevelItem = 'Hitze_Stufe';
 
-// load timer_mgr library
-this.OPENHAB_CONF = (this.OPENHAB_CONF === undefined) ? java.lang.System.getenv("OPENHAB_CONF") : this.OPENHAB_CONF
-load(OPENHAB_CONF+'/automation/lib/javascript/community/timerMgr.js')
-load(OPENHAB_CONF+'/automation/lib/javascript/community/groupUtils.js')
+/*
+Script starts here. Do not modify.
+*/
+this.OPENHAB_CONF = (this.OPENHAB_CONF === undefined) ? java.lang.System.getenv('OPENHAB_CONF') : this.OPENHAB_CONF;
+load(OPENHAB_CONF + '/automation/lib/javascript/community/timerMgr.js');
+load(OPENHAB_CONF + '/automation/lib/javascript/community/groupUtils.js');
 // Only create a new manager if one doesn't already exist or else it will be wiped out each time the rule runs
-this.tm = (this.tm === undefined) ? new TimerMgr() : this.tm
+this.tm = (this.tm === undefined) ? new TimerMgr() : this.tm;
 
-var logger = Java.type('org.slf4j.LoggerFactory').getLogger('org.openhab.rule.' + ctx.ruleUID)
-var NotificationAction = Java.type('org.openhab.io.openhabcloud.NotificationAction')
+var logger = Java.type('org.slf4j.LoggerFactory').getLogger('org.openhab.rule.' + ctx.ruleUID);
+var NotificationAction = Java.type('org.openhab.io.openhabcloud.NotificationAction');
 
-// send the notification
+/**
+ * Send the notification.
+ *
+ * @param {string} itemLabel The label of the item, therfore the name of the window/door.
+ */
 function NotificationWarning (itemLabel) {
-  // logger.info('Sending notification: ' + itemLabel + ' schließen, zu warm zum Lüften!')
-  NotificationAction.sendBroadcastNotification(itemLabel + ' schließen, zu warm zum Lüften!')
-  // NotificationAction.sendNotification('e-mail', itemLabel + ' schließen, zu warm zum Lüften!')
+  logger.debug('Sending notification: "' + itemLabel + ' schließen, zu warm zum Lüften!"');
+  NotificationAction.sendBroadcastNotification(itemLabel + ' schließen, zu warm zum Lüften!');
 }
 function NotificationAlarm (itemLabel) {
-  // logger.info('Sending notification: ' + 'Hitzealarm: ' + itemLabel + ' schließen, zu warm zum Lüften!')
-  NotificationAction.sendBroadcastNotification('Hitzealarm: ' + itemLabel + ' schließen, zu warm zum Lüften!')
-  // NotificationAction.sendNotification('e-mail', 'Hitzealarm: ' + itemLabel + ' schließen, zu warm zum Lüften!')
+  logger.debug('Sending notification: ' + 'Hitzealarm: "' + itemLabel + ' schließen, zu warm zum Lüften!"');
+  NotificationAction.sendBroadcastNotification('Hitzealarm: ' + itemLabel + ' schließen, zu warm zum Lüften!');
 }
 
-// check for the temperature difference
+/**
+ * Check whether the temperature difference from inside to outside is larger than tempTreshold.
+ * This works by extracting the roomname from the itemname and getting the temperature of that room.
+ *
+ * @param {string} contactItem Name of the contact item.
+ * @returns {boolean} Whether treshold is reached.
+ */
 function TemperatureDifference (contactItem) {
-  var tempInItem = contactItem.split('_')[0]
-  tempInItem = tempInItem + '_Temperatur'
+  var tempInItem = contactItem.split('_')[0];
+  tempInItem = tempInItem + '_Temperatur';
   // check whether item exists, if not -1 it is member of temperature group
-  var tempCheck = getMembersNames('Temperature').indexOf(tempInItem)
+  var tempCheck = getMembersNames('Temperature').indexOf(tempInItem);
   // calculate difference, inside to outside
-  if (tempCheck != -1) {
-    var tempIn = itemRegistry.getItem(tempInItem).getState()
-    var diff = tempOut - tempIn
+  if (tempCheck !== -1) {
+    var tempIn = itemRegistry.getItem(tempInItem).getState();
+    var diff = tempOut - tempIn;
     if (diff >= tempTreshold) {
-      var bool = true
+      var bool = true;
     } else {
-      var bool = false
+      var bool = false;
     }
   } else {
-    bool = true
+    bool = true;
   }
-  return bool
+  return bool;
 }
 
-// check the status and start the timer for the notification
+/**
+ * Alarm manager
+ * It checks the requirements and schedules an alarm.
+ * When an alarm is already scheduled, no new alarm will be scheduled.
+ *
+ * @param {string} contactItem Name of the contact item.
+ * @param {number} timerDuration Minutes for the timer.
+ */
 function StartWarning (contactItem, timerDuration) {
-  var heatLevel = itemRegistry.getItem(heatLevelItem).getState().toString()
-  var itemLabel = itemRegistry.getItem(contactItem).getLabel()
-  var diff = TemperatureDifference(contactItem)
+  var heatLevel = itemRegistry.getItem(heatLevelItem).getState().toBigDecimal();
+  var itemLabel = itemRegistry.getItem(contactItem).getLabel();
+  var diff = TemperatureDifference(contactItem);
+  // Check heat level and temperature difference before scheduling alarm.
   if ((heatLevel >= 1) && (diff === true)) {
     // Function generator for the timerOver actions.
     function timerOver (contactItem, itemLabel, heatLevelItem) {
-      logger.info('Creating timer "' + contactItem + '" time: ' + timerTime)
+      logger.info('Creating timer "' + contactItem + '" time: ' + timerTime);
       return function () {
-        var contactState = itemRegistry.getItem(contactItem).getState().toString()
-        var heatLevel = itemRegistry.getItem(heatLevelItem).getState().toString()
-        var diff = TemperatureDifference(contactItem)
-        logger.info('The timer is over. Contact item is: '+ contactItem + ' temp treshold reached: ' + diff)
+        var contactState = itemRegistry.getItem(contactItem).getState().toString();
+        var heatLevel = itemRegistry.getItem(heatLevelItem).getState().toBigDecimal();
+        var diff = TemperatureDifference(contactItem);
+        logger.info('The timer is over. Contact item is: ' + contactItem + ' temp treshold reached: ' + diff);
+        // Check heat level and temperature difference on alarm execution.
         if ((contactState === 'CLOSED') && (diff === true)) {
-          if (heatLevel == 4) {
-            NotificationAlarm(itemLabel)
+          if (heatLevel === 4) {
+            NotificationAlarm(itemLabel);
           } else if (heatLevel >= 1) {
-            NotificationWarning(itemLabel)
+            NotificationWarning(itemLabel);
           }
         }
-      }
+      };
     }
-    // Generate the timer time, if heatLevel is not 4 (alarm) then add time
-    if (heatLevel == 4) {
-      var timerTime = timerDuration + 'm'
+    // Generate the timer time, if heatLevel is not 4 (alarm) add time.
+    if (heatLevel === 4) {
+      var timerTime = timerDuration + 'm';
     } else {
-      var timerTime = timerDuration + warningTime + 'm'
+      var timerTime = timerDuration + warningTime + 'm';
     }
     // Create the Timer
     if (this.tm.hasTimer(contactItem)) {
-      logger.info('Timer for "' + contactItem + '" already exists, skipping!')
+      logger.debug('Timer for "' + contactItem + '" already exists, skipping!');
     } else {
-      this.tm.check(contactItem, timerTime, timerOver(contactItem, itemLabel, heatLevelItem))
+      this.tm.check(contactItem, timerTime, timerOver(contactItem, itemLabel, heatLevelItem));
     }
   }
 }
 
-// Rainalarm for roofwindow. The state of the window is built out of three contacts. Called with the room name.
+/**
+ * Get the state of a roowindow and call the alarm manager.
+ * When a window is closed, cancel the alarm.
+ *
+ * @param {string} item Name of the item.
+ */
 function RoofwindowAlarm (item) {
   // remove the suffix
-  item = item.replace('_zu', '')
-  item = item.replace('_klLueftung', '')
-  item = item.replace('_grLueftung', '')
+  item = item.replace('_zu', '');
+  item = item.replace('_klLueftung', '');
+  item = item.replace('_grLueftung', '');
   // retrieve the contact states from openHAB
-  var StateZu = itemRegistry.getItem(item + '_zu').getState().toString()
-  var StateKlLueftung = itemRegistry.getItem(item + '_klLueftung').getState().toString()
-  var StateGrLueftung = itemRegistry.getItem(item + '_grLueftung').getState().toString()
+  var StateZu = itemRegistry.getItem(item + '_zu').getState().toString();
+  var StateKlLueftung = itemRegistry.getItem(item + '_klLueftung').getState().toString();
+  var StateGrLueftung = itemRegistry.getItem(item + '_grLueftung').getState().toString();
   // checks for the different states.
   if (StateZu === 'CLOSED' && StateKlLueftung === 'CLOSED' && StateGrLueftung === 'CLOSED') { // ganz geöffnet
-    StartWarning(item + '_zu', openTime)
+    StartWarning(item + '_zu', openTime);
   } else if (StateZu === 'CLOSED' && StateKlLueftung === 'CLOSED' && StateGrLueftung === 'OPEN') { // große Lüftung
-    StartWarning(item + '_zu', grLueftungTime)
+    StartWarning(item + '_zu', grLueftungTime);
   } else if (StateZu === 'CLOSED' && StateKlLueftung === 'OPEN' && StateGrLueftung === 'OPEN') { // kleine Lüftung
-    StartWarning(item + '_zu', klLueftungTime)
+    StartWarning(item + '_zu', klLueftungTime);
   } else if (StateZu === 'OPEN') { // ganz geschlossen
     if (this.tm.hasTimer(item + '_zu')) {
-      logger.info('Cancelling timer for "' + item + '_zu", contact closed.')
-      this.tm.cancel(item + '_zu')
+      logger.info('Cancelling timer for "' + item + '_zu", contact closed.');
+      this.tm.cancel(item + '_zu');
     }
   }
 }
 
-// Heatalarm for windows with a single contact. Called with the contact item.
+/**
+ * Get the state of a normal window or door and call the alarm manager.
+ * When a window is closed, cancel the alarm.
+ *
+ * @param {string} contactItem Name of the item.
+ */
 function SingleContact (contactItem) {
   // retrieve the contact state from openHAB
-  var StateSingle = itemRegistry.getItem(contactItem).getState().toString()
-  if (StateSingle === 'CLOSED') {
-    StartWarning(contactItem, openTime)
-  } else if (StateSingle === 'OPEN') {
+  var stateSingle = itemRegistry.getItem(contactItem).getState().toString();
+  if (stateSingle === 'CLOSED') {
+    StartWarning(contactItem, openTime);
+  } else if (stateSingle === 'OPEN') {
     if (this.tm.hasTimer(contactItem)) {
-      logger.info('Cancelling timer for "' + contactItem + '", contact closed.')
-      this.tm.cancel(contactItem)
+      logger.info('Cancelling timer for "' + contactItem + '", contact closed.');
+      this.tm.cancel(contactItem);
     }
   }
 }
 
-var groupMembers = getMembersNames(contactGroup)
+var groupMembers = getMembersNames(groupname);
 for (var index in groupMembers) {
-  // check whether itemname contains 'Dachfenster'
-  var b = groupMembers[index].search('Dachfenster')
-  if (b != -1) {
-    // logger.info('checking roofindow: ' + groupMembers[index])
-    RoofwindowAlarm(groupMembers[index])
+  // Check whether itemname contains variable roofwindowString.
+  var b = groupMembers[index].search(roofwindowString);
+  if ((b !== -1) &&
+  (groupMembers[index] !== 'Treppenhaus_Dachfenster_zu')) { // Additional check for my special window that closes automatically on rain.
+    logger.debug('Checking roofindow: ' + groupMembers[index]);
+    RoofwindowAlarm(groupMembers[index]);
   } else {
-    // logger.info('checking single contact: ' + groupMembers[index])
-    SingleContact(groupMembers[index])
+    logger.debug('Checking single contact: ' + groupMembers[index]);
+    SingleContact(groupMembers[index]);
   }
 }
