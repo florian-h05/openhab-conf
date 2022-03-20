6c2a760a31743edefb63dd2342995746c79cfcee
Florian Hotze <florianh_dev@icloud.com>
2022-03-20 14:57:18 +0100

    GraalJS: Update thing-status rule for higher flexibility.

--- automation/js/thing-status.js
+++ automation/js/thing-status.js
@@ -1,25 +1,48 @@
 /*
 This script sends a thing's state to an openHAB item.
 
-Items must follow this naming scheme: "<binding>:<...>:<name>"" with Item "<binding>_<name>_state"
-e.g. for "knx:device:bridge:Wallswitch" Item "KNX_Wallswitch_state"
+Items' names must end with "_state". 
+The Item "ThingState_Refresh" is required.
 
 Copyright (c) 2021 Florian Hotze under MIT License
 */
 
 const { actions, items, rules, triggers } = require('openhab');
-let triggersList = [triggers.SystemStartlevelTrigger(100), triggers.GenericCronTrigger('0 0/5 * * * *')];
+let triggersList = [
+  triggers.ItemCommandTrigger('ThingState_Refresh', 'ON'),
+  triggers.SystemStartlevelTrigger(100), 
+  triggers.GenericCronTrigger('0 0/5 * * * *')
+];
+
+/**
+ * Get a Thing's name from the Item name.
+ * Always "_state" is removed first.
+ * Next, patterns is replaced with replaces. If patterns is an Array, replaces must be an Array, search-replace pairs must have the same index.
+ * @param {String} itemname Item's name
+ * @param {String|Array<String>} patterns Patterns to replace.
+ * @param {String|Array<String>} replaces String that is used for replacing.
+ */
+const getThingName = (itemname, patterns, replaces) => {
+  if (typeof patterns === 'string') return itemname.replace('_state', '').replaceAll(patterns, replaces);
+  // When patterns and replaces are Arrays.
+  let thingname = itemname.replace('_state', '');
+  for (const i in patterns) {
+    thingname = thingname.replaceAll(patterns[i], replaces[i])
+  };
+  return thingname;
+};
 
 /**
  * Send a Thing's state to an openHAB Item.
+ * Replaces all "_" with ":" and removes "_state".
  * @param {String} groupName name of the group
- * @param {String} thingPrefix thing id's prefix before the device name
- * @param {String} itemPrefix state item's prefix
+ * @param {String|Array<String>} patterns Patterns to replace.
+ * @param {String|Array<String>} replaces String that is used for replacing.
  */
-const updateThingStatus = (groupName, thingPrefix, itemPrefix) => {
+const updateThingStatus = (groupName, patterns, replaces) => {
   const members = items.getItem(groupName).members.map(item => item.name);
   for (let i = 0; i < members.length; i++) {
-    const thing = thingPrefix + members[i].replace('_state', '').replace(itemPrefix, '');
+    const thing = getThingName(members[i], patterns, replaces);
     const thingStatus = String(actions.Things.getThingStatusInfo(thing));
     items.getItem(members[i]).postUpdate(thingStatus);
   }
@@ -27,34 +50,34 @@ const updateThingStatus = (groupName, thingPrefix, itemPrefix) => {
 
 /**
  * Returns the rule triggers.
+ * Replaces all "_" with ":" and removes "_state".
  * @param {String} groupName name of the group
- * @param {String} thingPrefix thing id's prefix before the device name
- * @param {String} itemPrefix state item's prefix
+ * @param {String|Array<String>} patterns Patterns to replace.
+ * @param {String|Array<String>} replaces String that is used for replacing.
  */
-const getTriggers = (groupName, thingPrefix, itemPrefix) => {
+const getTriggers = (groupName, patterns, replaces) => {
   const members = items.getItem(groupName).members.map(item => item.name);
   for (let i = 0; i < members.length; i++) {
-    const thing = thingPrefix + members[i].replace('_state', '').replace(itemPrefix, '');
+    const thing = getThingName(members[i], patterns, replaces);
     triggersList.push(triggers.ThingStatusChangeTrigger(thing));
   }
 };
 
-getTriggers('KNXState', 'knx:device:bridge:', 'KNX_');
-getTriggers('YamahaState', 'yamahareceiver:yamahaAV:', 'Yamaha_');
-triggersList.push(triggers.ThingStatusChangeTrigger('knx:ip:bridge:'));
-triggersList.push(triggers.ItemCommandTrigger('ThingState_Refresh', 'ON'));
+/* CONFIGURE RULE HERE ------------------------------------------------------------------------------------- */
+
+getTriggers('KNXState', ['KNX_IP_Gateway', 'KNX_'], ['knx:ip:bridge', 'knx:device:bridge:']);
+getTriggers('YamahaState', '_', ':');
 
 rules.JSRule({
   name: 'Thing State to Item',
   description: 'Send the things \' states on change to a state item.',
   triggers: triggersList,
   execute: data => {
-    updateThingStatus('KNXState', 'knx:device:bridge:', 'KNX_');
-    updateThingStatus('YamahaState', 'yamahareceiver:yamahaAV:', 'Yamaha_');
-    // KNX IP Gateway
-    const thingStatus = String(actions.Things.getThingStatusInfo('knx:ip:bridge'));
-    items.getItem('KNX_IP_Gateway_state').postUpdate(thingStatus);
+    updateThingStatus('KNXState', ['KNX_IP_Gateway', 'KNX_'], ['knx:ip:bridge', 'knx:device:bridge:']);
+    updateThingStatus('YamahaState', '_', ':');
     // Update refresh button
     items.getItem('ThingState_Refresh').postUpdate('OFF');
-  }
+  },
+  id: 'thing-state',
+  tags: ['System', 'Things', 'Status']
 });
